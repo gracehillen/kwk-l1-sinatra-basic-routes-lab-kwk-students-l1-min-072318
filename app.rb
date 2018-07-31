06239252718897443900d391711a123b3b72d5f6
gracehillen <grahillen19@ahastars.org>
2018-07-31 15:34:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,13 +8,16 @@ class App < Sinatra::Base
   get '/name' do
   "My name is Grace"
 end
+end
 
   get '/hometown' do
   "My hometown is Minnetonka"
+end
 end
 
   get '/favorite-song' do
   "My favorite song is God's Plan"
 end
+end 
 
 end
